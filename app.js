72f8e2e2143cbc045531d92afb690779225064a0
Shinji Hasei <tohoho@xug.biglobe.ne.jp>
2019-12-08 23:04:50 +0900

    commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function hello() {
-    document.querySelectorAll("#message").innerHTML = "hello, world";
-}
+window.addEventListener("load", function() {
+    document.getElementById("hello").addEventListener("click", function() {
+        document.getElementById("message").innerHTML += "Hello, ";
+    });
+    document.getElementById("bye").addEventListener("click", function() {
+        document.getElementById("message").innerHTML += "Bye, ";
+    });
+    document.getElementById("exit").addEventListener("click", function() {
+        window.close();
+    });
+});
+
